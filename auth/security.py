c34bf7ab71edc6f90d29da7a81d96c39c9b8c744
dinaa <elmedinabucolli1612@gmail.com>
2026-06-02 21:41:03 +0200

    auth

--- auth/security.py
+++ auth/security.py
@@ -1,14 +1,18 @@
-from pydantic import BaseModel
+from fastapi import FastAPI, Depends, HTTPException, status
+from fastapi.security import APIKeyHeader
+from dotenv import load_dotenv
+import os
 
-class DriverBase(BaseModel):
-    name: str
+load_dotenv()
+API_KEY = os.getenv("API_KEYS")
+API_KEY_NAME = "api-key"
+api_key_header = APIKeyHeader(name=API_KEY_NAME, auto_error=False)
 
-class DriverCreate(DriverBase):
-    pass
 
-class DriverResponse(BaseModel):
-    id: int
-    name: str
-
-class Driver(DriverBase):
-    id: int
+def get_api_key(api_key: str = Depends(api_key_header)):
+    if api_key != API_KEY:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid API Key",
+        )
+    return api_key
