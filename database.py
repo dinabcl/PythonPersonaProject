73e0cb77f7796b47940bac3c8e1e6ca2cf34e3ff
dinaa <elmedinabucolli1612@gmail.com>
2026-06-02 22:35:12 +0200

    ...

--- database.py
+++ database.py
@@ -1,1 +1,22 @@
+import sqlite3
 
+def get_db_connection():
+    conn = sqlite3.connect("taxi.db")
+    conn.row_factory = sqlite3.Row
+    return conn
+
+def create_database():
+    conn = sqlite3.connect("taxi.db")
+    cursor = conn.cursor()
+
+    cursor.execute('''CREATE TABLE IF NOT EXISTS taxi (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        taxi TEXT,
+        driver_id INTEGER,
+        ride_id INTEGER,
+        costumer_id INTEGER,
+        pickup_location TEXT,
+        destination TEXT,
+        status TEXT,
+        FOREIGN KEY(driver_id) REFERENCES drivers(id)
+    )''')
